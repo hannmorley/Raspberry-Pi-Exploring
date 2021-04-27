7ebb8c2bd979006314798e028a1defc3989248b0
jrrussell750 <46250525+jrrussell750@users.noreply.github.com>
2021-04-27 10:58:48 -0500

    Update cwrapper.c

--- cwrapper.c
+++ cwrapper.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <python2.7/Python.h>
+//
+//  This module demonstrates how Python code can be executed inside of a C wapper.
+//  By having a self-contained executable, you avoid the contention issues caused by multiple includes 
+//  having duplicate definitions
+//
 // Use this command to compile me: gcc cwrapper.c -I/usr/include/python2.7 -lpython2.7
 
 int main()
